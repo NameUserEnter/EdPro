f5f52e4a12a015782a4c943ba19dfc64fbcce5f4
NameUserEnter <tomredl2003@gmail.com>
2023-03-03 17:44:16 +0200

    email

---
+++ EdPro/Models/EmailService.cs
@@ -0,0 +1,30 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+namespace EdPro.Models
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "robotpro@ukr.net"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.ukr.net", 465, true);
+                await client.AuthenticateAsync("robotpro@ukr.net", "GkddMlahpqPD4dLY");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
